4fd88a9d06846cd52f8a595e960508127f0196db
Kevin Tereza <kevinterezadev@gmail.com>
2026-08-27 22:13:07 -0300

    commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Mainlayout } from './components/mainlayout/mainlayout';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Mainlayout, // Este é o componente que tem o header, footer e o router-outlet
+    children: [
+      { 
+        path: '', 
+        component: Home // Quando acessar '/', carrega o Mainlayout E o HomeComponent dentro dele
+      },
+      { 
+        path: 'about', 
+        component: About // Quando acessar '/', carrega o Mainlayout E o HomeComponent dentro dele
+      },
+    //   { 
+    //     path: 'produtos', 
+    //     component: ProdutosComponent // Quando acessar '/produtos', carrega o Mainlayout E a tela de produtos
+    //   }
+    ]
+  },
+  {
+    path: '**', 
+    redirectTo: '' // Se digitar qualquer rota errada, joga de volta pra home
+  }
+];
